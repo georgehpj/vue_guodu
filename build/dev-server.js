ce180fa9faed8bb182148ca964740fc148fd1ff9
georgehpj <hupeijiegeorge@hotmail.com>
2016-08-08 10:00:08 +0800

    登录页面开发

--- build/dev-server.js
+++ build/dev-server.js
@@ -51,8 +51,7 @@ app.use(devMiddleware)
 app.use(hotMiddleware)
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsP
-ublicPath, config.dev.assetsSubDirectory)
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
 module.exports = app.listen(port, function (err) {
